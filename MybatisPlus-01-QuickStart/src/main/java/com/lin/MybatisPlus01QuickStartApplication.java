09b0b5d3c884fdfb1bededa388335043955beea1
LinMouren <lgl13727753294@163.com>
2022-06-21 11:18:35 +0800

    quickstart

--- MybatisPlus-01-QuickStart/src/main/java/com/lin/MybatisPlus01QuickStartApplication.java
+++ MybatisPlus-01-QuickStart/src/main/java/com/lin/MybatisPlus01QuickStartApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.lin.mapper")
+@MapperScan(basePackages = {"com.lin.mapper"})
 public class MybatisPlus01QuickStartApplication {
 
     public static void main(String[] args) {
--- MybatisPlus-01-QuickStart/src/main/java/com/lin/mapper/UserMapper.java
+++ MybatisPlus-01-QuickStart/src/main/java/com/lin/mapper/UserMapper.java
@@ -3,7 +3,9 @@ package com.lin.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lin.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 
